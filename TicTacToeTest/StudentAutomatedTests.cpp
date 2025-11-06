175fc4103c2342aa5d89d031872425d459d00ed1
Joshua Lynch <135474581+Joshua-Lynch@users.noreply.github.com>
2025-11-05 21:12:11 -0500

    Added test cases during meeting for practice

--- TicTacToeTest/StudentAutomatedTests.cpp
+++ TicTacToeTest/StudentAutomatedTests.cpp
@@ -30,5 +30,40 @@ namespace TicTacToeTest
 			Logger::WriteMessage("Running student tests on the Board object");
 			Assert::IsTrue(true);
 		}
+
+		TEST_METHOD(VerifySomeCellsIsEmpty)
+		{
+			Logger::WriteMessage("Checking if cell 1,1 is empty");
+			Assert::IsTrue(board.isSquareEmpty(1, 1)); 
+		}
+
+		TEST_METHOD(VerifyWriteACell)
+		{
+			Logger::WriteMessage("Checking if we can write/read"); 
+			Assert::IsTrue(board.isSquareEmpty(1, 1)); 
+			board.writeSquare(1, 1, TicTacToeBoard::X); 
+			Assert::IsTrue(board.isSquareEmpty(1, 1));
+			Assert::AreEqual('X',board.getSquareContents(1, 1)); 
+			Assert::AreEqual('O', board.getSquareContents(1, 1));
+		}
+
+		TEST_METHOD(PlayerXWins)
+		{
+			Logger::WriteMessage("Testing if the game can identify a Win");
+			Assert::IsFalse(board.isWinner(TicTacToeBoard::X));
+			Assert::IsFalse(board.isWinner(TicTacToeBoard::O));
+			Assert::IsFalse(board.isDraw());
+
+			//Checking if the winning row is in the middle of the board
+			board.writeSquare(1, 0, TicTacToeBoard::X);
+			board.writeSquare(1, 1, TicTacToeBoard::X);
+			board.writeSquare(1, 2, TicTacToeBoard::X);
+
+			Assert::IsTrue(board.isWinner(TicTacToeBoard::X));
+			Assert::IsFalse(board.isWinner(TicTacToeBoard::O));
+			Assert::IsFalse(board.isDraw());
+
+
+		}
 	};
 }
