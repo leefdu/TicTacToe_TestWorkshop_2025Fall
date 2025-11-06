5284d17d226d42aad5b77a0868bccc4dd7b37f6d
Skimlk <vanhoutencocoapowder@gmail.com>
2025-11-05 21:13:19 -0500

    CLASS-680-2025F - Add VerifySomeCellIsEmpty, VerifyWriteACell, VerifyXWins, TestSwitchingPlayers tests

--- TicTacToeTest/StudentAutomatedTests.cpp
+++ TicTacToeTest/StudentAutomatedTests.cpp
@@ -30,5 +30,52 @@ namespace TicTacToeTest
 			Logger::WriteMessage("Running student tests on the Board object");
 			Assert::IsTrue(true);
 		}
+
+		TEST_METHOD(VerifySomeCellIsEmpty) {
+			Logger::WriteMessage("Checking if cell 1,1 is empty");
+			Assert::IsTrue(board.isSquareEmpty(1, 1));
+			Assert::IsFalse(board.getSquareContents(1, 1) == NULL);
+		}
+
+		TEST_METHOD(VerifyWriteACell) {
+			Logger::WriteMessage("Checking if we can write/read a cell");
+			
+			Assert::IsTrue(board.isSquareEmpty(1, 1));
+
+			board.writeSquare(1, 1, TicTacToeBoard::X);
+
+			Assert::IsFalse(board.isSquareEmpty(1, 1));
+			Assert::AreEqual(board.getSquareContents(1, 1), 'X');
+			Assert::AreNotEqual(board.getSquareContents(1, 1), 'O');
+			Assert::AreNotEqual(board.getSquareContents(1, 1), ' ');
+		}
+
+		TEST_METHOD(VerifyXWins) {
+			Logger::WriteMessage("Check if X wins the game");
+
+			// Checking pre-conditions
+			Assert::IsFalse(board.isWinner(TicTacToeBoard::X));
+			Assert::IsFalse(board.isWinner(TicTacToeBoard::O));
+			Assert::IsFalse(board.isDraw());
+
+			// Writing X to middle row
+			board.writeSquare(1, 0, TicTacToeBoard::X);
+			board.writeSquare(1, 1, TicTacToeBoard::X);
+			board.writeSquare(1, 2, TicTacToeBoard::X);
+
+			// Checking if X won
+			Assert::IsTrue(board.isWinner(TicTacToeBoard::X));
+			Assert::IsFalse(board.isWinner(TicTacToeBoard::O));
+			Assert::IsFalse(board.isDraw());
+		}
+
+		TEST_METHOD(TestSwitchingPlayers) {
+			Logger::WriteMessage("Check if players switch correctly");
+
+			Assert::AreEqual(board.getPlayerName(), 'X');
+			board.nextPlayer();
+			Assert::AreEqual(board.getPlayerName(), 'O');
+			Assert::AreNotEqual(board.getPlayerName(), 'X');
+		}
 	};
 }
